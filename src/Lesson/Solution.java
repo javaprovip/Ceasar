4c8dd435404706ff4e2d9d396b39df64da289715
javaprovip <sergiokravchenko@gmail.com>
2022-05-13 11:00:58 +0300

    Second

--- src/Lesson/Solution.java
+++
@@ -1,7 +0,0 @@
-package Lesson;
-
-public class Solution {
-    public static void main(String[] args) {
-        System.out.println("good");
-    }
-}
---
+++ src/org/lesson/Solution.java
@@ -0,0 +1,6 @@
+package org.lesson;
+
+public class Solution {
+    public static void main(String[] args) {System.out.println("good");
+    }
+}
